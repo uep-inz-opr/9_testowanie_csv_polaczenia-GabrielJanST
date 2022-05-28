fa59beb9916267018d3dd1e81ab9f31c02bd3860
Gabriel <gabriel@air-gabriel.mynet>
2022-05-28 20:18:42 +0200

    proba kolejna

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ class MenadzerPolaczen:
     calls_dict_sum = dict()
     with open(self.filename, 'r') as fin:
       reader = csv.reader(fin, delimiter= ",")
-      #headers = next(reader)
+      headers = next(reader)
 
       for row in reader:
         from_subsr = int(row[0])
         if from_subsr not in calls_dict_sum:
-            max(calls_dict_sum[from_subsr],key=calls_dict_sum[from_subsr])
+            calls_dict_sum[from_subsr] = 0
+            calls_dict_sum[from_subsr] += 1 
     return calls_dict_sum
 
   def pobierz_najczesciej_dzwoniacego(self):
     return max(self.data_dict_sum, key= lambda x: x[1])
 
 if __name__ == "__main__":
-  nazwa_pliku = input()
-  mp = MenadzerPolaczen("phoneCalls.csv")
+  nazwa_pliku = "phoneCalls.csv"
+  mp = MenadzerPolaczen(nazwa_pliku)
   wynik = mp.pobierz_najczesciej_dzwoniacego()
   print (wynik)
