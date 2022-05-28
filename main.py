7d3a8d7fd894d8c6f672e948290c7edc6f75adee
Gabriel <gabriel@air-gabriel.mynet>
2022-05-28 19:21:26 +0200

    proba3

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import csv
 class MenadzerPolaczen:
   def __init__(self, filename):
     self.filenaCme = filename
-    self.data_dict = self.read_data('phoneCalls.csv')
+    self.data_dict = self.read_data('phoneCalls.csv', 'r')
 
   def read_data(self):
     calls_dict_sum = dict()
-    with open(self.filename, 'r') as fin:
+    with open(self.read_data) as fin:
       reader = csv.reader(fin, delimiter= ",")
       headers = next(reader)
 
       for row in reader:
         from_subsr = int(row[0])
         if from_subsr not in calls_dict_sum:
-          calls_dict_sum[from_subsr] = 0
-        calls_dict_sum[from_subsr].count
+            max(calls_dict_sum[from_subsr],key=calls_dict_sum[from_subsr])
     return calls_dict_sum
 
   def pobierz_najczesciej_dzwoniacego(self):
